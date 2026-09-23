be38a0614783c13a39046953da8467ffd8c6b2eb
Luis Abad <pablo.anchiraicoabad@gmail.com>
2026-09-23 04:04:45 -0500

    Agrega Screen.kt con rutas de navegacion

--- Clinica/app/src/main/java/com/abad/clinicasaludplus/navigation/Screen.kt
+++ Clinica/app/src/main/java/com/abad/clinicasaludplus/navigation/Screen.kt
@@ -1,2 +1,28 @@
 package com.abad.clinicasaludplus.navigation
 
+sealed class Screen(
+    val route: String
+) {
+    object Home : Screen("home")
+    object MisCitas : Screen("mis_citas")
+    object HistorialMedico : Screen("historial_medico")
+
+    object DoctorProfile : Screen(
+        "doctor_profile/{doctorId}"
+    ) {
+        fun createRoute(doctorId: Int) = "doctor_profile/$doctorId"
+    }
+
+    object AgendarCita : Screen(
+        "agendar_cita/{doctorId}"
+    ) {
+        fun createRoute(doctorId: Int) = "agendar_cita/$doctorId"
+    }
+
+    object Confirmacion : Screen(
+        "confirmacion/{doctorId}/{fecha}/{hora}"
+    ) {
+        fun createRoute(doctorId: Int, fecha: String, hora: String) =
+            "confirmacion/$doctorId/${android.net.Uri.encode(fecha)}/${android.net.Uri.encode(hora)}"
+    }
+}
